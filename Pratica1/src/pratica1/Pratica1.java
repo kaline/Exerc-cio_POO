0d32856078b6da19b294fb1118c66b7350df4daa
Alunos <Alunos>
2019-08-23 14:50:39 -0900

    adciona segundo projeto

---
+++ Pratica1/src/pratica1/Pratica1.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pratica1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Pratica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int a, b, i;
+        
+        System.out.println("Digite A:");
+        a = sc.nextInt();
+        System.out.println("Digite B:");
+        b = sc.nextInt();
+        
+        
+            system.out.println("escolha:");
+            i = sc.nextInt();
+            
+            switch(i){
+                case 1:
+                    int soma = a + b;
+                    System.out.println("soma" + soma);
+                    break;
+                case 2:
+                    System.out.println("Subtracao");
+                    break;
+                case 3:
+                    System.out.println("Multiplicaçao");
+                    break;
+                case 4:
+                    System.out.println("divisao");
+                    break;
+                   
+
+
+        
+        
+    }
+    
+    
+    
+}
